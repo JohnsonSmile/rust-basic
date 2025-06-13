baab53f58f112fae4959af0c0ea7b90b3b333eb9
JohnsonSmile <kevensmiler@163.com>
2025-06-13 18:53:06 +0800

    feat: 获取git日志

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{env, error::Error, fs, path::Path, process};
+use std::path::Path;
 
-use anyhow::{Ok, Result};
+use anyhow::{Result, bail};
+use chrono::DateTime;
 use git2::{
-    BranchType, CertificateCheckStatus, Cred, Direction, FetchOptions, RemoteCallbacks, Repository,
-    build,
+    Cred, FetchOptions, RemoteCallbacks, Repository, Sort,
+    build::{self},
 };
 
 // cargo run -- the poem.txt
@@ -19,9 +20,38 @@ use git2::{
 //     }
 // }
 
+struct Args {
+    /// git 的地址
+    remote_url: String,
+    /// 本地的 git 项目保存地址
+    local_path: String,
+    /// rsa地址
+    private_key_path: String,
+    /// 作者的名字
+    author_names: Vec<String>,
+}
+
 fn main() -> Result<()> {
-    let remote_url = "git@e.coding.net:fedtech/ares/erp_api.git";
-    let local_path = "repos/erp_api";
+    let args = Args {
+        remote_url: "git@e.coding.net:fedtech/ares/erp_api.git".to_string(),
+        local_path: "repos".to_string(),
+        private_key_path: "ssh/id_rsa".to_string(),
+        author_names: vec![
+            "mawanli".to_string(),
+            "马万里".to_string(),
+            "JohnsonSmile".to_string(),
+        ],
+    };
+
+    let remote_url = &args.remote_url;
+    let Some(project_name) = remote_url
+        .split('/')
+        .last()
+        .and_then(|s| Some(s.replace(".git", "")))
+    else {
+        bail!("解析失败")
+    };
+    let local_path = Path::new(&args.local_path).join(project_name); // "repos/erp_api";
 
     let repo = Repository::open(local_path)?;
     let mut remote = repo
@@ -31,7 +61,7 @@ fn main() -> Result<()> {
     // 创建克隆配置
     let mut cb = RemoteCallbacks::new();
     // 设置 SSH 凭据回调
-    cb.credentials(|url, username_from_url, allowed_types| {
+    cb.credentials(|_url, username_from_url, _allowed_types| {
         // SSH 私钥路径（通常在 ~/.ssh/id_rsa）
         let private_key = Path::new("ssh/id_rsa");
         // 公钥路径（通常在 ~/.ssh/id_rsa.pub）
@@ -47,16 +77,45 @@ fn main() -> Result<()> {
             passphrase,
         )
     });
-    // Connect to the remote and call the printing function for each of the
-    // remote references.
-    // let mut connection = remote.connect_auth(Direction::Fetch, Some(cb), None)?;
 
-    for branch in repo.branches(Some(BranchType::Remote))? {
-        let (branch_name, _) = branch?;
-        println!("  * {:?}", branch_name.name());
+    // fetch option
+    let mut fo = FetchOptions::new();
+    fo.remote_callbacks(cb);
+
+    let references = repo.references()?;
+    for reference in references {
+        let reference = reference?;
+        let Some(reference_name) = reference.name() else {
+            continue;
+        };
+        println!("reference_name: {:?}", reference_name);
+        repo.set_head(reference_name)?;
+        remote.fetch(&[reference_name], Some(&mut fo), None)?;
+
+        let mut revwalk = repo.revwalk()?;
+        revwalk.set_sorting(Sort::TIME)?;
+        revwalk.push_head()?;
+        for commit in revwalk {
+            let oid = commit?;
+            let commit = repo.find_commit(oid)?;
+            let author_name = commit.author().name().unwrap_or("unknown").to_string();
+            if args.author_names.contains(&author_name) {
+                println!("author_name: {:?}", author_name);
+                println!("commit: {:?}", commit.id());
+                println!(
+                    "commit time: {:?}",
+                    DateTime::from_timestamp(commit.time().seconds(), 0)
+                        .unwrap()
+                        .to_utc()
+                );
+                println!(
+                    "commit message: {:?}",
+                    commit.message().unwrap_or("unknown")
+                );
+            }
+        }
     }
 
-    // TODO:xxx
     return Ok(());
 
     // 创建克隆配置
@@ -64,7 +123,7 @@ fn main() -> Result<()> {
     // 设置 SSH 凭据回调
     cb.credentials(|url, username_from_url, allowed_types| {
         // SSH 私钥路径（通常在 ~/.ssh/id_rsa）
-        let private_key = Path::new("ssh/id_rsa");
+        let private_key = Path::new(&args.private_key_path);
         // 公钥路径（通常在 ~/.ssh/id_rsa.pub）
         let public_key = private_key.with_extension("pub");
         // 若密钥有密码，提供密码；否则设为 None
@@ -85,7 +144,6 @@ fn main() -> Result<()> {
     // Prepare builder.
     let mut builder = build::RepoBuilder::new();
     builder.fetch_options(fo);
-    let path = Path::new(local_path);
-    builder.clone(remote_url, path)?;
+    builder.clone(remote_url, &local_path)?;
     Ok(())
 }
